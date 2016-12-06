87786a960aced7434ef4e125519791fa0ae29424
Lars Lorch <Lars@larss-macbook-pro.local>
2016-12-06 18:47:07 -0500

    d

--- The Billboard/User.swift
+++ The Billboard/User.swift
@@ -17,6 +17,8 @@ class User
     var emailAddress: String
     var password: String
     
+    let x = 0
+    
     // Initialization
     init?(username: String, emailAddress: String, password: String)
     {
