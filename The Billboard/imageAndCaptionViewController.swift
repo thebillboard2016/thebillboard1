54a139de1e8266c3bab223bf27b4715f42a04845
Samuel Meijer <Sam@f.local>
2016-12-07 15:14:20 -0500

    Upload and save messages work, Save works, Upload still TODO

--- The Billboard/imageAndCaptionViewController.swift
+++ The Billboard/imageAndCaptionViewController.swift
@@ -30,12 +30,14 @@ class imageAndCaptionViewController: UIViewController, UIImagePickerControllerDe
         let imageData = UIImageJPEGRepresentation(takenImage.image!, 0.6)
         let compressedImage = UIImage(data: imageData!)
         //UPLOAD
+        uploadNotice()
     }
     
     @IBAction func saveBarButton(_ sender: UIBarButtonItem) {
         let imageData = UIImageJPEGRepresentation(takenImage.image!, 0.6)
         let compressedImage = UIImage(data: imageData!)
         UIImageWriteToSavedPhotosAlbum(compressedImage!, nil, nil, nil)
+        saveNotice()
     }
     
 
@@ -62,5 +64,22 @@ class imageAndCaptionViewController: UIViewController, UIImagePickerControllerDe
         takenImage.image = image
         self.dismiss(animated: true, completion: nil);
     }
+    
+    
+    //Notices for uploading and saving the image.
+    func uploadNotice(){
+        let alertController = UIAlertController(title: "Upload", message: "Your image has been uploaded", preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        present (alertController, animated: true, completion: nil)
+    }
+    
+    func saveNotice(){
+        let alertController = UIAlertController(title: "Save", message: "Your image has been Saved", preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        present (alertController, animated: true, completion: nil)
+    }
+    
 }
 
